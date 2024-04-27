7b8c59341d1067b11695b1bb20425a45034cf4b0
dayana <dayana.romero88@gmail.com>
2024-04-27 10:30:04 +0200

    ultima version

--- src/api/controllers/user.controller.js
+++ src/api/controllers/user.controller.js
@@ -1,7 +1,7 @@
 const User = require('../models/user.model');
 const bcrypt = require('bcrypt');
 const { validateEmailDB, validatePassword } = require('../../utils/validator');
-const { generateToken } = require("../../utils/jwt")
+const { generateToken } = require('../../utils/jwt');
 
 const register = async (req, res) => {
   try {
@@ -44,35 +44,48 @@ const register = async (req, res) => {
 const login = async (req, res) => {
   try {
     const userBody = req.body;
-    const userDB = await validateEmailDB(userBody.email)
+    const userDB = await validateEmailDB(userBody.email);
     if (!userDB) {
-      return res.status(200).json({ succe: false, message: "El email no está registrado" })
+      return res
+        .status(200)
+        .json({ succe: false, message: 'El email no está registrado' });
     }
     if (!bcrypt.compareSync(userBody.password, userDB.password)) {
-      return res.status(200).json({ succes: true, message: "contraseña invalida" })
+      return res
+        .status(200)
+        .json({ succes: true, message: 'contraseña invalida' });
     }
     //generar el token
     const token = generateToken({
       name: userDB.name,
       email: userDB.email,
       _id: userDB._id,
-    })
-    return res.status(200).json({ success: true, token: token })
-
+    });
+    return res.status(200).json({ success: true, token: token });
   } catch (error) {
     return res.status(500).json(error);
   }
-}
+};
 
 const modifyProfile = async (req, res) => {
-  console.log("funcion de modificar")
+  console.log('funcion de modificar');
   console.log(req.userProfile); // es el usuario con los datos correspondiente al token
   const newUser = new User(req.body);
-  newUser.password = bcrypt.hashSync(req.body.password, 10)
-  newUser._id = req.userProfile._id
-  console.log(newUser)
-  const updateUser = await User.findByIdAndUpdate(req.userProfile._id, newUser, { new: true })
-  return res.status(200).json({ data: updateUser })
-}
+  newUser.password = bcrypt.hashSync(req.body.password, 10);
+  newUser._id = req.userProfile._id;
+  console.log(newUser);
+  const updateUser = await User.findByIdAndUpdate(
+    req.userProfile._id,
+    newUser,
+    { new: true }
+  );
+  return res.status(200).json({ data: updateUser });
+};
+const getUsers = async (req, res) => {
+  try {
+    const usersDB = await User.find();
+    return res.json(usersDB);
+  } catch (error) {}
+};
 
-module.exports = { register, login, modifyProfile };
+module.exports = { register, login, modifyProfile, getUsers };
