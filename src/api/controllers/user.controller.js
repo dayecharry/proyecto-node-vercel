b4f0f986a99ce464776a521caa1201a3ffd7f3b2
dayecharry <124364526+dayecharry@users.noreply.github.com>
2024-07-17 20:06:04 +0200

    Update user.controller.js

--- src/api/controllers/user.controller.js
+++ src/api/controllers/user.controller.js
@@ -22,6 +22,7 @@ const register = async (req, res) => {
       //const valPassword = validatePassword(req.body.password);
       //if (valPassword) {
         //3.- Encriptar la contraseña  antes de registrarme  HASH
+      console.log(userDoc.password)
         userDoc.password = bcrypt.hashSync(userDoc.password, 10);
         const createdUser = await userDoc.save();
         return res.status(200).json({ success: true, data: createdUser });
